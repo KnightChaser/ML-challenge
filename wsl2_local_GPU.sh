cb318ccd4710f1caca3e481fdb18cde82abc95e7
Lee Garam <47494005+KnightChaser@users.noreply.github.com>
2023-06-20 00:28:03 +0900

    Update wsl2_local_GPU.sh

--- wsl2_local_GPU.sh
+++ wsl2_local_GPU.sh
@@ -1,26 +1,4 @@
-##### In my personal experience, it's pretty unstable or unpredictable to install Jupyter notebook
-##### and following NVIDIA's GPU kits on WSL2 (Windows 11). Proceed with caution and report anytime
-##### if you found errors during the following procedure by issuing on this repository.
-
-### In WSL2, Ubuntu
-### It's (highly) recommended to run commands one-by-one, not copying and pasting a bunch of commands that would generate some errors like "unexpected data stream while unpacking the following packages...".
-
-# apt update and install CUDA
-# Refer to the official CUDA installation webpage: https://developer.nvidia.com/cuda-toolkit-archive to get a bunch of the newest command sets.
-
-# Delete the old GPG key (if it's an appliance to you)
-sudo apt-key del 7fa2af80
-
-# Initiate installation process for CUDA
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
-sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda-repo-ubuntu2204-12-1-local_12.1.0-530.30.02-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2204-12-1-local_12.1.0-530.30.02-1_amd64.deb
-sudo cp /var/cuda-repo-ubuntu2204-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get -y install cuda
-
+############################## BASIC INSTALLATION ##############################
 
 # Install Python3 and Jupyter Notebook
 sudo apt-get install python3
@@ -30,11 +8,33 @@ sudo reboot
 
 # Restart your WSL2 after the installation process finishes
 
-# Run 
+# Run jupyter notebook
 jupyter notebook
 
-### In case of using docker to utilize external GPU(ex. NVIDIA RTX), use docker according to Google's official guide
-### https://research.google.com/colaboratory/local-runtimes.html
+#################################################################################
+
+# Basic installation finished (for CPUs/Intel-based internal GPUs)
+# For external GPUs provided by NVIDIA, you may proceed the following step (if you want).
+
+############################# NVIDIA GPU Utilization ############################
+# In my personal experience, it's pretty unstable or unpredictable to install Jupyter notebook
+# and following NVIDIA's GPU kits on WSL2 (Windows 11). Proceed with caution and report anytime
+# if you found errors during the following procedure by issuing on this repository.
+
+# Delete the old GPG key (if it's an appliance to you)
+sudo apt-key del 7fa2af80
+
+# Initiate installation process for CUDA
+# Refer to the official CUDA installation webpage: 
+# └ https://developer.nvidia.com/cuda-toolkit-archive to get a bunch of the newest command sets.
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda-repo-ubuntu2204-12-1-local_12.1.0-530.30.02-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2204-12-1-local_12.1.0-530.30.02-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2204-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get -y install cuda
 
 # install docker
 sudo apt-get install docker
@@ -53,6 +53,6 @@ sudo apt-get update
 sudo apt-get install -y nvidia-container-toolkit
 
 # run google collaboratory with docker (Bunch of downloads will be required if you are about to fresh start.)
-sudo docker run --gpus=all -p 127.0.0.1:9000:8080 us-docker.pkg.dev/colab-images/public/runtime
-
 # Now you will be able to utilize your GPU.
+sudo docker run --gpus=all -p 127.0.0.1:9000:8080 us-docker.pkg.dev/colab-images/public/runtime
+#################################################################################
