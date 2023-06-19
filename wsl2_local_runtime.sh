c8f4b383f141bc9dea7534766afb029874422661
Lee Garam <47494005+KnightChaser@users.noreply.github.com>
2023-06-19 12:52:43 +0900

    Update wsl2_local_runtime.sh

--- wsl2_local_runtime.sh
+++ wsl2_local_runtime.sh
@@ -1,16 +1,25 @@
-# In WSL2
+### In WSL2, Ubuntu
+
+# Delete old GPG key (if it's appliance to you)
+sudo apt-key del 7fa2af80
+
+# apt update and install CUDA
 wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
 sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/11.4.0/local_installers/cuda-repo-wsl-ubuntu-11-4-local_11.4.0-1_amd64.deb
-sudo dpkg -i cuda-repo-wsl-ubuntu-11-4-local_11.4.0-1_amd64.deb
-sudo apt-key add /var/cuda-repo-wsl-ubuntu-11-4-local/7fa2af80.pub
+wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda-repo-wsl-ubuntu-11-8-local_11.8.0-1_amd64.deb
+sudo dpkg -i cuda-repo-wsl-ubuntu-11-8-local_11.8.0-1_amd64.deb
+sudo cp /var/cuda-repo-wsl-ubuntu-11-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update
+sudo apt-get upgrade -y
 sudo apt-get -y install cuda
 
-# Install Jupyter 2 (over WSL)
-pip install jupyter_http_over_ws
-sudo apt install jupyter
-jupyter serverextension enable --py jupyter_http_over_ws
+# Install Python3 and Jupyter notebook
+sudo apt-get install python3
+sudo apt-get install python3-pip
+sudo pip3 install jupyter
+sudo reboot
 
-# Run with gpu
-docker run --gpus=all -p 127.0.0.1:9000:8080 us-docker.pkg.dev/colab-images/public/runtime
+# Restart your WSL2 after the installation process finishes
+
+# Run 
+Jupyter notebook
