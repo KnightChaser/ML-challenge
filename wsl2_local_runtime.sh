a3a1c2e36208e6418ac522b7dc6a87da68e9846d
Lee Garam <47494005+KnightChaser@users.noreply.github.com>
2023-06-19 14:50:01 +0900

    For Ubuntu 22.04 (latest)

--- wsl2_local_runtime.sh
+++ wsl2_local_runtime.sh
@@ -1,19 +1,24 @@
 ### In WSL2, Ubuntu
-
-# Delete old GPG key (if it's appliance to you)
-sudo apt-key del 7fa2af80
+### It's (highly) recommended to run commands one-by-one, not copying and pasting a bunch of commands that would generate some errors like "unexpected data stream while unpacking the following packages...".
 
 # apt update and install CUDA
-wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
-sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda-repo-wsl-ubuntu-11-8-local_11.8.0-1_amd64.deb
-sudo dpkg -i cuda-repo-wsl-ubuntu-11-8-local_11.8.0-1_amd64.deb
-sudo cp /var/cuda-repo-wsl-ubuntu-11-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
+# Refer to the official CUDA installation webpage: https://developer.nvidia.com/cuda-toolkit-archive to get a bunch of the newest command sets.
+
+# Delete the old GPG key (if it's an appliance to you)
+sudo apt-key del 7fa2af80
+
+# Initiate installation process for CUDA
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda-repo-ubuntu2204-12-1-local_12.1.0-530.30.02-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2204-12-1-local_12.1.0-530.30.02-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2204-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get -y install cuda
 
-# Install Python3 and Jupyter notebook
+
+# Install Python3 and Jupyter Notebook
 sudo apt-get install python3
 sudo apt-get install python3-pip
 sudo pip3 install jupyter
@@ -22,4 +27,4 @@ sudo reboot
 # Restart your WSL2 after the installation process finishes
 
 # Run 
-Jupyter notebook
+jupyter notebook
