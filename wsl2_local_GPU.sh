d0663642e736e85e5444c564ab0809b95ac6c288
Lee Garam <47494005+KnightChaser@users.noreply.github.com>
2023-06-19 19:25:45 +0900

    CUDA Toolkit on WSL2 Install Update

---
+++ wsl2_local_GPU.sh
@@ -0,0 +1,63 @@
+##### In my personal experience, it's pretty unstable or unpredictable to install Jupyter notebook
+##### and following NVIDIA's GPU kits on WSL2 (Windows 11). Proceed with caution and report anytime
+##### if you found errors during the following procedure by issuing on this repository.
+
+### In WSL2, Ubuntu
+### It's (highly) recommended to run commands one-by-one, not copying and pasting a bunch of commands that would generate some errors like "unexpected data stream while unpacking the following packages...".
+
+# To faster update, append this to /etc/apt/source.list
+deb mirror://mirrors.ubuntu.com/mirrors.txt precise main restricted universe multiverse
+deb mirror://mirrors.ubuntu.com/mirrors.txt precise-updates main restricted universe multiverse
+deb mirror://mirrors.ubuntu.com/mirrors.txt precise-backports main restricted universe multiverse
+deb mirror://mirrors.ubuntu.com/mirrors.txt precise-security main restricted universe multiverse
+
+# apt update and install CUDA
+# Refer to the official CUDA installation webpage: https://developer.nvidia.com/cuda-toolkit-archive to get a bunch of the newest command sets.
+
+# Delete the old GPG key (if it's an appliance to you)
+sudo apt-key del 7fa2af80
+
+# Initiate installation process for CUDA
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda-repo-ubuntu2204-12-1-local_12.1.0-530.30.02-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2204-12-1-local_12.1.0-530.30.02-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2204-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get -y install cuda
+
+
+# Install Python3 and Jupyter Notebook
+sudo apt-get install python3
+sudo apt-get install python3-pip
+sudo pip3 install jupyter
+sudo reboot
+
+# Restart your WSL2 after the installation process finishes
+
+# Run 
+jupyter notebook
+
+### In case of using docker to utilize external GPU(ex. NVIDIA RTX), use docker according to Google's official guide
+### https://research.google.com/colaboratory/local-runtimes.html
+
+# install docker
+sudo apt-get install docker
+sudo apt-get install docker.io
+
+# If you need NVIDIA CUDA Toolkit, don't forget to install the tool too.
+# Ref. https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html#setting-up-nvidia-container-toolkit
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+      && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+            
+sudo apt-get update
+sudo apt-get install -y nvidia-container-toolkit
+
+# run google collaboratory with docker (Bunch of downloads will be required if you are about to fresh start.)
+sudo docker run --gpus=all -p 127.0.0.1:9000:8080 us-docker.pkg.dev/colab-images/public/runtime
+
+# Now you will be able to utilize your GPU.
