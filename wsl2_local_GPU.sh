d5763fa480d0069a87f1683feb6f7bc6698a6274
Lee Garam <47494005+KnightChaser@users.noreply.github.com>
2023-07-02 22:06:58 +0900

    Update wsl2_local_GPU.sh

--- wsl2_local_GPU.sh
+++ wsl2_local_GPU.sh
@@ -32,8 +32,7 @@ sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda-repo-ubuntu2204-12-1-local_12.1.0-530.30.02-1_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu2204-12-1-local_12.1.0-530.30.02-1_amd64.deb
 sudo cp /var/cuda-repo-ubuntu2204-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/
-sudo apt-get update
-sudo apt-get upgrade -y
+sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get -y install cuda
 
 # install docker
